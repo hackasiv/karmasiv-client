e000294da98034da9225d3c560ba5a7a3bb37d50
Pavel Gordeev <mitridat2000@gmail.com>
2015-12-10 14:24:07 +0300

    angular handler

--- www/scripts/application.js
+++ www/scripts/application.js
@@ -1,8 +1,50 @@
-angular.module('SteroidsApplication', [
-  'supersonic'
-])
-.controller('IndexController', function($scope, supersonic) {
+var karma = angular.module('SteroidsApplication', [
+  'supersonic',
+  'infinite-scroll'
+]);
+
+karma.controller('IndexController', function($scope, supersonic, Karma) {
+  
+  $scope.search_term = '';
 
   $scope.navbarTitle = "Welcome to Supersonic!";
+  
+  $scope.Karma = new Karma();
+  
+});
 
+// Reddit constructor function to encapsulate HTTP and pagination logic
+karma.factory('Karma', function($http) {
+    
+  var Karma = function() {
+    this.items = [];
+    this.busy = false;
+    this.after = '';
+    this.page = 1;
+    this.items_per_page = 20;
+    this.search_term = 'Пушкин';
+  };
+
+  Karma.prototype.nextPage = function() {
+    if (this.busy) return;
+    this.busy = true;
+
+    var url = "http://env-0112901.j.layershift.co.uk/?q=" + this.search_term;
+    
+//    alert($http.get(url))
+    
+    $http.get(url).then(function(data) {
+        alert(data);
+      var items = data;
+      for (var i = 0; i < items.length; i++) {
+        this.items.push(items[i].data);
+      }
+      this.after = this.items[this.items.length - 1].id;
+      this.busy = false;
+    }.bind(this), function(e){
+        alert(e.message)
+    });
+  };
+
+  return Karma;
 });
