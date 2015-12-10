66518730b451d17cdad90cb6786f6d8aa5055342
Pavel Gordeev <mitridat2000@gmail.com>
2015-12-10 15:14:16 +0300

    fix

--- www/scripts/application.js
+++ www/scripts/application.js
@@ -6,6 +6,7 @@ var karma = angular.module('SteroidsApplication', [
 karma.config(function($httpProvider) {
     //Enable cross domain calls
     $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
 });
 
 
@@ -36,16 +37,17 @@ karma.factory('Karma', function($http) {
     this.busy = true;
 
     var url = "http://env-0112901.j.layershift.co.uk/?q=" + this.search_term;
-    
+    $http.defaults.useXDomain = true;
 //    alert($http.get(url))
     
     $http.get(url).then(function(data) {
       console.log(data);
-      var items = data;
+      var items = data.data;
+      
       for (var i = 0; i < items.length; i++) {
         this.items.push(items[i].data);
       }
-      this.after = this.items[this.items.length - 1].id;
+//      this.after = this.items[this.items.length - 1].id;
       this.busy = false;
     }.bind(this), function(e){
         console.log(e)
